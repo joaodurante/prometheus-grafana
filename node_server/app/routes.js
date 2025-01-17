2c2b57ecb34316dd1515197683465927eb428a2b
Joao Pedro De Munno Durante <jaodurante@gmail.com>
2025-01-17 09:40:47 -0300

    code improvements

--- node_server/app/routes.js
+++ node_server/app/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const promClient = require('./promClient');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const sendMetrics = async () => {
     // Start measuring the duration for this request
     const histogramEnd = promClient.requestDurationHistogram.startTimer();
     const summaryEnd = promClient.requestDurationSummary.startTimer();
@@ -15,11 +15,21 @@ router.get('/', (req, res) => {
 
     // Simulate some processing and random response times
     setTimeout(() => {
-        res.send('Hello, World!');
         histogramEnd();
         summaryEnd();
         promClient.requestsActive.dec();
     }, Math.random() * 1000);
+}
+
+// Timer to simulate user requests
+setInterval(async () => {
+    await sendMetrics();
+}, 100);
+
+// GET request
+router.get('/', async (req, res) => {
+    await sendMetrics();
+    res.send('Hello, World!');
 });
 
 // Define the /metrics route where Prometheus will scrape the metrics
